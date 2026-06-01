9ac7bc74e70fde20b5a93d13fc76e4f6bf2a7d0c
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:14:25 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for Bouncing-Ball-Game-JavaFX.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:14:24Z'
+});
